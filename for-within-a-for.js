83bd53092b8fd4b7b4c636793fb584a844825707
TheSanches <saanches7@gmail.com>
2024-01-05 18:41:12 +0200

    work with arr

--- for-within-a-for.js
+++ for-within-a-for.js
@@ -1,15 +1,15 @@
-let result = '';
-const length = 7;
+// let result = '';
+// const length = 7;
 
-for(let i = 0; i < length; i++) {
-    for(let j = 0; j < i; j++) {
-        result += '*';
-    }
+// for(let i = 0; i < length; i++) {
+//     for(let j = 0; j < i; j++) {
+//         result += '*';
+//     }
 
-    result += '\n';
-}
+//     result += '\n';
+// }
 
-console.log(result);
+// console.log(result);
 
 // *
 // **
@@ -30,3 +30,17 @@ first: for(let i = 0; i < 3; i++) {
 }
 
 //https://www.udemy.com/course/javascript_full/learn/lecture/30882734#overview
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+// for(let i = data.length - 1; i >= 0; i--) {
+//     result.push(data[i]);
+// }
+for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i]
+}
+
+console.log(result)
+
+// [ 'Homework', 20, 'Shopping', 10, 5 ]
